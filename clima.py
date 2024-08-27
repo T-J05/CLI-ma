244050cfaf4c98781499b0fc0e0c369206acef97
Jose Toledo <bernardinotoledo6@gmail.com>
2024-08-27 15:51:53 -0400

    se creo el archivo clima.py

---
+++ clima.py
@@ -0,0 +1,16 @@
+# Creación de la Aplicación CLI
+
+# Usa una librería adecuada para gestionar los argumentos y comandos de la CLI.
+
+# Implementa una estructura que permita recibir y procesar argumentos, 
+# definiendo cuáles son requeridos y cuáles opcionales.
+# Incluye un mensaje de ayuda (--help) que explique cómo usar la aplicación y describa los
+# diferentes comandos disponibles.
+
+# Filtrado por Ubicación
+# Permite al usuario especificar una ubicación (Nombre de ciudad - País) mediante un argumento
+# de línea de comandos. Configura la lógica para recibir y validar este argumento.
+# La aplicación debe hacer una solicitud a una API de clima con la ubicación proporcionada, filtrar
+# los datos recibidos y mostrar información relevante, como temperatura y condiciones
+# meteorológicas.
+# Permite también al usuario elegir el formato de salida, como JSON, CSV o texto plano.
