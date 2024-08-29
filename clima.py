6b3e13b9abe1c3dbbcfbb6903f60adc45ad1b8b3
Jose Toledo <bernardinotoledo6@gmail.com>
2024-08-29 14:06:15 -0400

    clima.py refactorizado

--- clima.py
+++ clima.py
@@ -1,49 +1,63 @@
-#!/usr/bin/env python3
 # Creación de la Aplicación CLI
-
 # Usa una librería adecuada para gestionar los argumentos y comandos de la CLI.
 from dotenv import load_dotenv
 import os
 import requests
 import argparse
+import json
 # Carga el archivo .env
 load_dotenv()
 # Accede a la API key usando el mismo nombre de variable
 api_key = os.getenv('API_KEY')
 
-parser = argparse.ArgumentParser(description="Aplicacion para ver el clima desde la linea de comando" )
 
-parser.add_argument("ciudad",type=str,help="Ingrese la ciudad")
-parser.add_argument("tipo",type=str, nargs="?",default="json",help="Ingrese el formato json/texto")
+def clima():
+    parser = argparse.ArgumentParser(description="Aplicacion para ver el clima desde la linea de comando" )
 
-argumentos = parser.parse_args()
+    parser.add_argument("ciudad",type=str,help="Ingrese la ciudad")
+    parser.add_argument("tipo",type=str, nargs="?",default="json",help="Ingrese el formato json/texto")
 
-ciudad = argumentos.ciudad
-tipo = argumentos.tipo
-# creamos un diccionario que con los parametros para la url
-parametros = {"q":ciudad,
-              "units":"metric",
-              "APPID":api_key}
+    argumentos = parser.parse_args()
 
-# realizamos la peticion, indicando la url y los parametros
-respuesta= requests.get("http://api.openweathermap.org/data/2.5/weather",params=parametros)
+    ciudad = argumentos.ciudad
+    tipo = argumentos.tipo
+    # creamos un diccionario que con los parametros para la url
+    parametros = {
+        "q": ciudad,
+        "units": "metric",
+        "APPID": api_key
+    }
+    try:
+        # Realizamos la peticion, indicando la url y los parametros
+        respuesta= requests.get("http://api.openweathermap.org/data/2.5/weather",params=parametros)
+        # si la respuesta devuelve 200 es por que todo salio bien
+        if respuesta.status_code == 200:
+            # La respuesta json se convierte en un diccionario
+            datos = respuesta.json()
+            datos_json = datos["main"]  # nos adentramos a la clave main del diccionario
+            # traducimos todo a espanhol
+            jsonn = { 
+                    "temperatura": f"{datos_json['temp']} ºC",
+                    "sensacion_termica": f"{datos_json['feels_like']} ºC",
+                    "temperatura_minima": f"{datos_json['temp_min']} ºC",
+                    "temperatura_maxima": f"{datos_json['temp_max']} ºC",
+                    "presion": f"{datos_json['pressure']} hPa",
+                    "humedad": f"{datos_json['humidity']}%"
+            }
+            json_simple = json.dumps(jsonn, ensure_ascii=False, indent=4)  # covertimos el diccionario en json simple para su lectura
+            if tipo == "json":  # imprime en json
+                print(json_simple)
 
-# si la respuesta devuelve 200 es por que todo salio bien
-if respuesta.status_code == 200:
-# La respuesta json se convierte en un diccionario
-    datos = respuesta.json()
-    if tipo == "json":
-        datos_json = datos["main"]
-        print(datos_json)
-    # Se obtienen los valores del diccionario
-    elif tipo == "texto":
-        print("La temperatura actual es:",datos["main"]["temp"],"ºC")
-        print("La sensación térmica es:",datos["main"]["feels_like"],"ºC")
-        print("La temperatura mínima es:",datos["main"]["temp_min"],"ºC")
-        print("La temperatura máxima es:",datos["main"]["temp_max"],"ºC")
-        print("La presión es:",datos["main"]["pressure"],"hPa")
-        print("La humedad es:",datos["main"]["humidity"],"%")
-    else:
-        print('Error formato no admitido o mal escrito: ')
-else:
-    print("No tengo datos sobre esa ciudad revisa si la ortografia es correcta")
+            elif tipo == "texto":  # imprime en texto
+                texto = "\n".join([f"{clave}: {valor}" for clave, valor in jsonn.items()]) 
+                print(texto)
+            else:
+                print('Error formato no admitido o malcescrito.')
+        else:
+            print("No tengo datos sobre esa ciudad revisa si la ortografia es correcta")
+    except requests.RequestException as e:
+        print(f"Error en la solicitud {e}")
+
+
+if __name__ == "__main__":  # para evitar que si se importa el archivo se ejecute
+    clima()
